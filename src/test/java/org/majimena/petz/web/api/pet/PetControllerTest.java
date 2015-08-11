3bae6f9765fd1be0ad70e0ef5a6f48df0f4acacd
Ken Todoroki <ken.todoroki@majimena.org>
2015-08-12 01:42:19 +0900

    travis test

--- src/test/java/org/majimena/petz/web/api/pet/PetControllerTest.java
+++ src/test/java/org/majimena/petz/web/api/pet/PetControllerTest.java
@@ -71,7 +71,7 @@ public class PetControllerTest {
 
             mockMvc.perform(post("/api/v1/pets")
                 .contentType(TestUtils.APPLICATION_JSON_UTF8)
-                .content("{\"birthDate\":\"2015-02-27T15:00:00.000Z\",\"user\":{\"id\":\"1\"},\"sex\":\"MALE\",\"tags\":[\"タグ２\",\"タグ１\"],\"types\":[\"タイプ１\",\"タイプ２\"],\"name\":\"ポチ\",\"profile\":\"プロファイル\"}"))
+                .content("{\"birthDate\":\"2015-02-27T15:00:00.000+09:00\",\"user\":{\"id\":\"1\"},\"sex\":\"MALE\",\"tags\":[\"タグ２\",\"タグ１\"],\"types\":[\"タイプ１\",\"タイプ２\"],\"name\":\"ポチ\",\"profile\":\"プロファイル\"}"))
 //                .content("{\"user\":{\"id\":\"1\"},\"sex\":\"MALE\",\"tags\":[\"タグ２\",\"タグ１\"],\"types\":[\"タイプ１\",\"タイプ２\"],\"name\":\"ポチ\",\"profile\":\"プロファイル\"}"))
                 .andDo(print())
                 .andExpect(status().isCreated());

3eee96443dba784b6ab85362688c29b92df611fc
Ken Todoroki <ken.todoroki@majimena.org>
2016-07-29 14:25:25 +0900

    bug fixed

--- petical-domain/src/main/java/org/majimena/petical/repository/ClinicInspectionRepository.java
+++ petical-domain/src/main/java/org/majimena/petical/repository/ClinicInspectionRepository.java
@@ -3,6 +3,7 @@ package org.majimena.petical.repository;
 import org.majimena.petical.domain.ClinicInspection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,10 +14,11 @@ import java.util.List;
  */
 public interface ClinicInspectionRepository extends JpaRepository<ClinicInspection, String>, JpaSpecificationExecutor<ClinicInspection> {
 
-    @Query(value = "insert into clinic_inspection "
-            + "select uuid(), :clinicId, course, category, name, price, insurance, unit, tax_type, tax_rate, description, false, :userId, now(), :userId, now() "
+    @Query(value = "insert into clinic_inspection (id, clinic_id, course, category, name, price, insurance, unit, tax_type, tax_rate, description, created_by, created_date, last_modified_by, last_modified_date) "
+            + "select uuid(), :clinicId, course, category, name, price, insurance, unit, tax_type, tax_rate, description, :userId, now(), :userId, now() "
             + "from inspection order by id", nativeQuery = true)
-    void setup(@Param("clinicId") String clinicId, @Param("userId") String userId);
+    @Modifying
+    int setup(@Param("clinicId") String clinicId, @Param("userId") String userId);
 
     List<ClinicInspection> findByClinicId(String clinicId);
 
